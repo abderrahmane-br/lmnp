b88c4bf95882e60f0dfd00ee190f29c3b12241b5
Abderrahmane-Br <abderrahmane.brahim.etd@univ-tiaret.dz>
2025-07-08 12:50:53 +0100

    feat: add photos section and carousel

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,8 @@ import PostsList from "@/components/PostsList";
 import Carousel from "@/components/Carousel";
 import Header from "@/components/Header";
 import ServicesList from "@/components/ServicesList";
-import SectionHeader from "@/components/SectionHeader";
+import SectionHeader from "@/components/SectionHeader"
+import PhotosCarousel from "@/components/PhotosCarousel";
 import { PICTURE, LIST, POSTS, STORE } from "@/components/icons";
 
 
@@ -19,6 +20,10 @@ export default function Home() {
         <Section >
           <SectionHeader icon={STORE} title={'À propos'} />
           Spécialiste des montages financiers et fiscaux en Loueur meublé non professionnel (LMNP) neuf et de seconde main, le cabinet LMNP Conseils vous accompagne dans votre projet de A à Z. Appelez LMNP Conseils pour échanger avec des experts depuis plus de 20 ans. Appel et consultation patrimoniale et fiscale gratuits et sans engagement. 📍 Accès à toutes les opportunités en LMNP 📦 Programmes neufs et revente ✅ Sélection rigoureuse et conseils d’experts
+        </Section>
+        <Section >
+          <SectionHeader icon={PICTURE} title={'Photos'} />
+          <PhotosCarousel />
         </Section>
         <Section id="services">
           <SectionHeader icon={LIST} title={'Services'} />
---
+++ src/components/PhotosCarousel.jsx
@@ -0,0 +1,96 @@
+"use client"
+import Link from "next/link";
+import { useEffect, useState } from 'react'
+import { supabase } from '../lib/supabaseClient'
+import CarouselArrow from "./CarouselArrow";
+import Image from "next/image";
+import styles from "@/styles/components/PhotosCarousel.module.scss"
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function PhotosCarousel() {
+  const [photos, setPhotos] = useState([])
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      setIsLoading(true)
+      const { data, error } = await supabase.schema('gmb_ads').from('annonces').select('id, img_url').is("publié", false)
+      if (error) {
+        console.error(error)
+      } else {
+        setPhotos(data)
+        setCurrentSlide(0)
+      }
+      setIsLoading(false)
+    }
+
+    fetchPhotos()
+  }, [])
+
+  const sliderSettings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    // arrows: true,
+    // centerMode: true,
+    centerPadding: '0px',
+    initialSlide: currentSlide,
+    nextArrow: <CarouselArrow right={true} />,
+    prevArrow: <CarouselArrow  />,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 2,
+            swipeToSlide: true,
+            touchThreshold: 10,
+            slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 565,
+        settings: {
+            slidesToShow: 1,
+            swipeToSlide: true,
+            touchThreshold: 10,
+            slidesToScroll: 1,
+            centerMode: true,
+            arrows: false
+        }
+      }
+    ]
+  }
+
+  if (isLoading) {
+    return null; // or a loading spinner
+  }
+
+  return (
+    <>
+      <div className={`${styles.carousel}`}>
+        <Slider {...sliderSettings} key={`slider-${photos.length}`}>
+          {photos.map((post) => (
+            <div key={post.id} >
+              <Image src={post.img_url} width={400} height={400} className={`${styles.photo}`}/>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </>
+  );
+}
+
+export default PhotosCarousel;
