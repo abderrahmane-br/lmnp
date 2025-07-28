6e19ed51afa8cc6c6c051b5282ae5cecf246ff83
Abderrahmane-Br <abderrahmane.brahim.etd@univ-tiaret.dz>
2025-07-28 16:51:43 +0100

    fix: fix default card header

--- src/components/PostCardHeader.jsx
+++ src/components/PostCardHeader.jsx
@@ -5,13 +5,14 @@ import { NEWSPAPER } from "./icons";
 import { Icon } from "@iconify/react";
 
 
-function PostCardHeader({header ='', date = '06-2025'}) {
+function PostCardHeader({header ='LMNP Conseils - Conseils en investissements locatifs', date = '06-2025'}) {
   return (
     <div className={`${styles.postCardHeader}`}>
         {/* <Image src={Logo} alt="Logo" className={`${styles.avatar}`} /> */}
         <div>
           <div>{header}</div>
           {/* {header ? <h2>{header}</h2> : <div>LMNP Conseils - Conseils en investissements locatifs</div>} */}
+          {/* <div>{header ? {header} : "LMNP Conseils - Conseils en investissements locatifs"}</div> */}
           <span className={`${styles.date}`}>{date}</span>
         </div>
         <Icon icon={NEWSPAPER} className={`${styles.icon}`}/>
