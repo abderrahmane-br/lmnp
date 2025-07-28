097173114d3cd3db15605441a937a492d8698a07
Abderrahmane-Br <abderrahmane.brahim.etd@univ-tiaret.dz>
2025-07-28 16:44:32 +0100

    update: put titre in card header

--- src/components/PostCardRich.jsx
+++ src/components/PostCardRich.jsx
@@ -14,13 +14,13 @@ function PostCardRich(post) {
 
   return (
     <article className={`${styles.postCard}`} onClick={() => handlePostClick(post)}>
-        <PostCardHeader date={post.date_prevue.slice(0, 10)}/>
+        <PostCardHeader date={post.date_prevue.slice(0, 10)} header={post.titre}/>
         <Image src={post.img_url ? post.img_url : FallbackImg} alt="illustration" className={`${styles.illustr}`} width={300} height={100}/>
 
         {/* <img src={post.img_url ? post.img_url : FallbackImg} alt="illustration" className={`${styles.illustr}`} /> */}
         <div className={`${styles.postContainer}`}>
           <div className={`${styles.post}`}>
-            <h2>{post.titre}</h2>
+            {/* <h2>{post.titre}</h2> */}
             <span><Markdown>{post.introduction}</Markdown></span>
             {
                 post.contenu.map(el => (
