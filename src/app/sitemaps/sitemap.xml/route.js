eb87a4baa128bcedfd405c064a6d090c5008fcef
Abderrahmane-Br <abderrahmane.brahim.etd@univ-tiaret.dz>
2025-07-30 11:02:42 +0100

    update: update sitemap

--- src/app/sitemaps/sitemap.xml/route.js
+++ src/app/sitemaps/sitemap.xml/route.js
@@ -6,10 +6,7 @@ export async function GET() {
   // Exemple statique ; ou générez dynamiquement via Supabase si besoin
   const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <sitemap>
-        <loc>${base}/page-sitemap.xml</loc>
-        <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
-    </sitemap>
+    
     <sitemap>
         <loc>${base}/actualites-sitemap.xml</loc>
         <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
