f67a004d849a3a6b85d6989e8c8e449e97ba106a
Abderrahmane-Br <abderrahmane.brahim.etd@univ-tiaret.dz>
2025-07-31 15:32:51 +0100

    fix: update sitemap index

--- src/app/sitemaps/sitemap.xml/route.js
+++ src/app/sitemaps/sitemap.xml/route.js
@@ -1,7 +1,20 @@
 import { NextResponse } from 'next/server';
+import { supabase } from "@/lib/supabaseClient";
+
 
 export async function GET() {
   const base = 'https://lmnp-conseils.immo';
+  const {data, error} = await supabase.rpc('get_distinct_months');
+
+  let dates = []
+  if (!error) {
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth() + 1;
+    const tDates = data.filter((d) => (d.year <= year) && (d.month <= month));
+    dates = tDates;
+  }
+
+
 
   // Exemple statique ; ou générez dynamiquement via Supabase si besoin
   const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
@@ -10,6 +23,12 @@ export async function GET() {
         <loc>${base}/page-sitemap.xml</loc>
         <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
     </sitemap>
+    ${dates.map( ({ year, month }) =>
+    `<sitemap>
+            <loc>${base}/actualites-sitemap-${year}-${month}.xml</loc>
+            <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+      </sitemap>`).join('')
+}
 </sitemapindex>`;
 
   return new NextResponse(sitemapIndex, {
