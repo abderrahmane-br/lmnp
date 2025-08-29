8ecd828f1bc63fb7d755f8341b5a6fcedf54182d
Abderrahmane-Br <abderrahmane.brahim.etd@univ-tiaret.dz>
2025-08-29 14:21:03 +0100

    update: remove noscript fallback for gtag

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,8 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4R5XW79"
-        height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
+        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4R5XW79"
+        height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript> */}
         <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
